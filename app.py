e6410c0192dc937abcb39b337eaab8cfa1b90f39
emily <emilycaroline.dev@gmail.com>
2026-05-21 16:06:09 -0300

    final

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 from flask import Flask, render_template, request
+import math
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     resultado = None
 
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        try:
-            conta = float(request.form['conta'])
-            consumo = float(request.form['consumo'])
-            custo_sistema = float(request.form['custo'])
+        consumo = float(request.form["consumo"])
+        conta = float(request.form["conta"])
 
-            # Economia fixa de 90%
-            economia = 90
+        # quantidade de placas
+        placas = math.ceil(consumo / 50)
 
-            economia_mensal = conta * (economia / 100)
-            economia_anual = economia_mensal * 12
+        # custo médio
+        custo = placas * 3000
 
-            retorno_anos = custo_sistema / economia_anual
+        # economia máxima
+        economia_mensal = conta * 0.90
 
-            resultado = {
-                'economia_mensal': round(economia_mensal, 2),
-                'economia_anual': round(economia_anual, 2),
-                'retorno_anos': round(retorno_anos, 1)
-            }
+        # economia anual
+        economia_anual = economia_mensal * 12
 
-        except:
-            resultado = 'Erro ao calcular.'
+        # retorno do investimento
+        retorno = custo / economia_anual
 
-    return render_template('index.html', resultado=resultado)
+        resultado = {
+            "placas": placas,
+            "custo": round(custo, 2),
+            "economia_mensal": round(economia_mensal, 2),
+            "economia_anual": round(economia_anual, 2),
+            "retorno": round(retorno, 1)
+        }
 
-if __name__ == '__main__':
+    return render_template("index.html", resultado=resultado)
+
+if __name__ == "__main__":
     app.run(debug=True)
